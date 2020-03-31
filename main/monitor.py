ad78780561efae2cde801c881e63eae2d86ef24f
Matthew Clark <winescout@gmail.com>
2020-03-31 08:37:21 -0700

    moves to IRQ instead of a loop to watch washing events

--- main/monitor.py
+++ main/monitor.py
@@ -9,55 +9,43 @@ def post_url():
 def update_url(id):
     return 'http://insubstantial-squeaky-pupa.gigalixirapp.com/api/washings/sessions/' + str(id) +'/end'
 
-def monitor():
-    active_request = False
-    inactive_noticings = 0
-    active_noticings = 0
-    led_active = False
-    pir_pin = 27
+def wash_start(pir_pin):
     led_pin_numbers = [12, 13, 14, 25, 26, 32]
     led_pins = map(lambda number: Pin(number, Pin.OUT), led_pin_numbers)
     success_seconds = 20
-    per_second = 3
+    seconds_active = 0
 
-    Pin(12, Pin.OUT).off()
+    #Make sure all the pins are off
     for pin in led_pin_numbers:
-        print("TURNING OFF %s" %pin)
         Pin(pin, Pin.OUT).off()
 
+    #Notify server washing event is active
+    response = urequests.post(post_url(), headers = {"content-type": "application/json", "content-length": "0"}).json()
+    end_url = update_url(response['id'])
 
+    #Monitor washing event
+    v = pir_pin.value()
+    while v == 1:
+        seconds_active += 1
 
-    while True:
-        sensor_active = Pin(pir_pin, Pin.IN).value() > 0 
-        # debounce activation
-        if (sensor_active):
-            active_noticings += 1
+        #Lite LEDs
+        pins_to_light = (seconds_active / success_seconds) * len(led_pin_numbers)
+        if(pins_to_light <= len(led_pin_numbers)):
+            for index in range(0, pins_to_light):
+                Pin(led_pin_numbers[index], Pin.OUT).on()
 
-            seconds_active = active_noticings / per_second 
-            print("Seconds Active %s" % seconds_active)
-            pins_to_light = (seconds_active / success_seconds) * len(led_pin_numbers)
+        #Take a rest, then grab a new value
+        time.sleep(1)
+        v = pir_pin.value()
 
-            if(pins_to_light <= len(led_pin_numbers)):
-                for index in range(0, pins_to_light):
-                    print("TURNING ON %s" % led_pin_numbers[index])
-                    Pin(led_pin_numbers[index], Pin.OUT).on()
+    #Washing complete, notify server
+    urequests.put(end_url, headers = {"content-type": "application/json", "content-length": "0"}).json()
 
-        #debounce inactivity
-        if (not sensor_active and active_request and end_url):
-            inactive_noticings += 1
+    #Turn off LED's
+    for pin in led_pin_numbers:
+        Pin(pin, Pin.OUT).off()
 
-        if active_noticings >= 3 and not active_request:
-            response = urequests.post(post_url(), headers = {"content-type": "application/json", "content-length": "0"}).json()
-            end_url = update_url(response['id'])
-            active_request = True
-
-        if inactive_noticings >= 2:
-            response = urequests.put(end_url, headers = {"content-type": "application/json", "content-length": "0"}).json()
-            active_request = False
-            inactive_noticings = 0
-            active_noticings = 0
-            for pin in led_pin_numbers:
-                print("TURNING OFF %s" %pin)
-                Pin(pin, Pin.OUT).off()
-
-        time.sleep(1 / per_second)
+def monitor():
+    pir_pin_number = 27
+    pir_pin = Pin(pir_pin_number, Pin.IN)
+    pir_pin.irq(trigger=Pin.IRQ_RISING, handler=wash_start)
